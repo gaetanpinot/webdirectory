0c2dd0933c6a4ea82164090f222b03a63f59121c
GaetanPinot <gaetan.pinot6@etu.univ-lorraine.fr>
2024-06-14 09:19:45 +0200

    rename template view

--- admin/src/conf/bootstrap.php
+++ admin/src/conf/bootstrap.php
@@ -12,7 +12,7 @@ $app = AppFactory::create();
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, false, false);
 $app=(require_once __DIR__ . '/../conf/routes.php')($app);
-$twig=Twig::create(__DIR__.'/../templates',
+$twig=Twig::create(__DIR__.'/../app/views',
     ['cache'=>__DIR__.'/../cache',
         'auto_reload'=> true]);
 
