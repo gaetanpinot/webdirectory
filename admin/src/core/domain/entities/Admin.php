449fc7fd94387a3a034ce565a57c147c212ca1e1
GaetanPinot <gaetan.pinot6@etu.univ-lorraine.fr>
2024-06-14 11:07:18 +0200

    admin entite

---
+++ admin/src/core/domain/entities/Admin.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace web\admin\core\domain\entities;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model
+{
+
+    use HasUuids;
+    protected $table = 'admin_user';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+    public $incrementing = false;
+
+}
