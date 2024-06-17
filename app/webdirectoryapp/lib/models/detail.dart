e00fd476ee39f2f26c6331f28afdc5fb3aa8f445
YxallaxY <maximekoler04@gmail.com>
2024-06-17 11:03:11 +0200

    Mofication fonct 1 avec api et Ajout de fonct 4

--- app/webdirectoryapp/lib/models/detail.dart
+++ app/webdirectoryapp/lib/models/detail.dart
@@ -1,15 +1,52 @@
-import 'package:webdirectoryapp/models/personne.dart';
+import 'package:webdirectoryapp/models/service.dart';
 
 class Detail {
-  final Personne personne;
-  final String url;
+  final int id;
+  final String nom;
+  final String prenom;
+  final Service service;
+  late String personneUrl;
 
-  Detail({required this.personne, required this.url});
+  Detail({
+    required this.id,
+    required this.nom,
+    required this.prenom,
+    required this.service,
+    String? personneUrl,
+  }) {
+    this.personneUrl = personneUrl ?? '';
+  }
 
   factory Detail.fromJson(Map<String, dynamic> json) {
     return Detail(
-      personne: Personne.fromJson(json['personne']),
-      url: json['url'],
-    );
+        id: json['id'],
+        nom: json['nom'],
+        prenom: json['prenom'],
+        service: Service.fromJson(json['service']),
+        personneUrl: json['links']['detail']);
+  }
+
+  String getNom() {
+    return nom;
+  }
+
+  getPrenom() {
+    return prenom;
+  }
+
+  getServiceLibelle() {
+    return service.libelle;
+  }
+
+  getServiceUrl() {
+    return service.serviceUrl;
+  }
+
+  getPersonneUrl() {
+    return personneUrl;
+  }
+
+  getId() {
+    return id;
   }
 }
--- app/webdirectoryapp/lib/models/service.dart
+++ app/webdirectoryapp/lib/models/service.dart
@@ -1,9 +1,13 @@
 class Service {
   final String libelle;
   final int id;
-  Service({required this.libelle, required this.id});
+  final String serviceUrl;
+  Service({required this.libelle, required this.id, required this.serviceUrl});
 
   factory Service.fromJson(Map<String, dynamic> json) {
-    return Service(libelle: json['libelle'], id: json['id']);
+    return Service(
+        libelle: json['libelle'],
+        id: json['id'],
+        serviceUrl: json['links']['detail']);
   }
 }
