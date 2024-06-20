3013f67057255d4b01715f521cebf38a28090cc0
GaetanPinot <gaetan.pinot6@etu.univ-lorraine.fr>
2024-06-20 08:41:06 +0200

    docketu status

--- web/src/lib/config.js
+++ web/src/lib/config.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-export const URL_API_BASE = 'http://localhost:44000';
 
 
-=======
 export const URL_API_BASE = 'http://docketu.iutnc.univ-lorraine.fr:44000';
->>>>>>> bd0ae2a (docketu server)
 export const URL_API_PERSONNES = URL_API_BASE + '/api/personnes?sort=nom-asc';
